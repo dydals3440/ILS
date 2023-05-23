63fb66e724f41a461062586d9b3250dbaba20632
김용민 <dydals3440@gmail.com>
2023-05-23 22:09:35 +0900

    feat: router structrue

--- src/index.js
+++ src/index.js
@@ -3,15 +3,54 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ErrorPage } from './pages/ErrorPage';
+import { Home } from './pages/Home';
+import { AllProducts } from './pages/AllProducts';
+import { ShoppingCart } from './pages/ShoppingCart';
+import { Likes } from './pages/Likes';
+import { NewProducts } from './pages/NewProducts';
+import { ProductDetail } from './pages/ProductDetail';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/products',
+        element: <AllProducts />,
+      },
+      {
+        path: '/products/:id',
+        element: <ProductDetail />,
+      },
+      {
+        path: '/products/new',
+        element: <NewProducts />,
+      },
+      {
+        path: '/carts',
+        element: <ShoppingCart />,
+      },
+      {
+        path: '/likes',
+        element: <Likes />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
